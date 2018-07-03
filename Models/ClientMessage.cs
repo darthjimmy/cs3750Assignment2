55814ee590d6568dc74e79724f76041bde38b7a5
Eric Smith <erics@dakcs.com>
2018-07-02 21:26:35 -0600

    Finish Client message

--- Models/ClientMessage.cs
+++ Models/ClientMessage.cs
@@ -1,9 +1,21 @@
 
+using System.Drawing;
+
 namespace Conway
 {
+    public class Location
+    {
+        public int X { get; set;}
+        public int Y { get; set; }
+    }
+
     public class ClientMessage
     {
         public string Command { get; set; }
-        public bool[,] Data {get;set;}
+        
+        public Color Color { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
